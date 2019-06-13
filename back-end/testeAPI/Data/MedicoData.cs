391f6fb1c8e2de2146d6b02f22eac5711542fb60
Marco <marco.filho@spassu.com.br>
2019-06-13 09:20:21 -0300

    new implementations

--- back-end/testeAPI/Data/MedicoData.cs
+++ back-end/testeAPI/Data/MedicoData.cs
@@ -1,7 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using testeAPI.Models;
+using testeAPI.Models.DTO;
+
 namespace testeAPI.Data
 {
   public class MedicoData
   {
+    internal RetornoPaginacao GetMedicosByFilter(MedicosFilterDTO medicos, int pg, int itensPg, string sort, int sortOrder)
+    {
+      throw new NotImplementedException();
+    }
 
+    internal Task AddMedico(Medico medico)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal bool CRMExists(int crm)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal bool MedicoExists(int id)
+    {
+      throw new NotImplementedException();
+    }
   }
 }
