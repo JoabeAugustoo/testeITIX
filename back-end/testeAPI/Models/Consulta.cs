82693d7e4eb12a1d73b4edcbcbc1a38aed4fb352
Marco Antonio <m.barceloslima@gmail.com>
2019-06-15 17:38:48 -0300

    implementação de modelo de banco

--- back-end/testeAPI/Models/Consulta.cs
+++ back-end/testeAPI/Models/Consulta.cs
@@ -1,17 +1,33 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace testeAPI.Models
 {
+    [Table("Consulta")]
     public class Consulta
     {
+        [Key]
+        [Column("codConsulta")]
         public int Id { get; set; }
+
+        [Column("nomePaciente")]
         public string NomePaciente { get; set; }
+
+        [Column("dataNascimento")]
         public DateTime DataNascimento { get; set; }
+
+        [Column("dataInicialConsulta")]
         public DateTime DataInicialConsulta { get; set; }
+
+        [Column("dataFinalConsulta")]
         public DateTime DataFinalConsulta { get; set; }
-        public int? CodMedicoAtendimento { get; set; }
+
+        [Column("observacoes")]
         public string Observacoes { get; set; }
-        public int CodMedico { get; set; }
+
+        [Column("codMedico")]
+        public int MedicoId { get; set; }
 
     }
 
--- back-end/testeAPI/Models/Medico.cs
+++ back-end/testeAPI/Models/Medico.cs
@@ -1,10 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace testeAPI.Models
 {
-  public class Medico
-  {
-    public int Id { get; set;}
-    public string Nome { get; set; }
-    public int CRM { get; set; }
-    public string Especialidade { get; set; }
-  }
+    [Table("Medico")]
+    public class Medico
+    {
+        [Key]
+        [Column("codMedico")]
+
+        public int Id { get; set; }
+
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("crm")]
+        public int CRM { get; set; }
+
+        [Column("especialidade")]
+        public string Especialidade { get; set; }
+    }
 }
