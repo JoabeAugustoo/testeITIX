781408cc6a0444c1926876a88eb408fcc765a05b
Marco Antonio <m.barceloslima@gmail.com>
2019-06-16 22:24:03 -0300

    ajustes

--- front-end/testeWeb/src/app/app-routing.module.ts
+++ front-end/testeWeb/src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PMVAuthenticationGuard, PMVAuthorizationGuard } from 'pmv-common';
-
-import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
-    path: '', canActivateChild: [PMVAuthenticationGuard, PMVAuthorizationGuard], children: [
-
-      { path: 'home', component: HomeComponent },
-      { path: 'processo', loadChildren: 'app/pages/processo/processo.module#ProcessoModule' },
-      { path: 'pendencia', loadChildren: 'app/pages/pendencia/pendencia.module#PendenciaModule' },
-      { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    path: '', children: [
+      { path: 'consulta', loadChildren: 'app/pages/pendencia/pendencia.module#PendenciaModule' },
+      { path: '**', redirectTo: 'consulta', pathMatch: 'full' },
     ]
   },
 ];
