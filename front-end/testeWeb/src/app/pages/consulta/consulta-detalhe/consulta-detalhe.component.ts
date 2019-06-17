55f130b9927fb6d6b21d23cad16a198a3cc69023
Marco <marco.filho@spassu.com.br>
2019-06-17 10:16:55 -0300

    ajustes

--- front-end/testeWeb/src/app/pages/consulta/consulta-detalhe/consulta-detalhe.component.ts
+++ front-end/testeWeb/src/app/pages/consulta/consulta-detalhe/consulta-detalhe.component.ts
@@ -1,21 +1,14 @@
 import { Component, OnInit, ViewChild, } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
-import { ConfirmDialogService, NotificationService, Combobox, PMVAuthenticationService } from 'pmv-common';
 import { DetailComponent } from '../../../shared/interfaces/detail-component.interface';
-import { PendenciaService } from '../../../services/pendencia.service';
-import { PesquisarPendencias } from '../../../models/pesquisar-pendencias.model';
-import { Processo_Pendencia } from '../../../models/processo-pendencia.model';
-import { SIPADProcesso } from '../../../models/sipad-processo.model';
-import { SIPADInteressadoProcesso } from '../../../models/sipad-interessado-processo.model';
-import { getSituacaoPendenciaAsList, SituacaoPendenciaEnum, getSituacaoPendenciaDescById } from '../../../models/enums/situacao-pendencia.enum';
-import { Processo_PendenciaAnexo } from '../../../models/processo-pendencia-anexo.model';
-import { Processo_PendenciaDocumentoSolicitado } from '../../../models/processo-pendencia-documento-solicitado.model';
-import { SituacaoDocumentoEnum } from '../../../models/enums/situacao-documento.enum';
 import { FormType } from '../../../models/enums/form-type.enum';
-import { PendenciaDetalheDadosComponent } from './pendencia-detalhe-dados/pendencia-detalhe-dados.component';
 import { ConsultaDetalheDadosComponent } from './consulta-detalhe-dados/consulta-detalhe-dados.component';
 import { ConsultaService } from 'src/app/services/consulta-service';
+import { ConfirmDialogService } from 'src/app/services/confirm-dialog.service';
+import { NotificationService } from 'src/app/services/notification-service';
+import { RetornoConsultasDTO } from 'src/app/models/DTO/retorno-consultas.dto';
+import { Consulta } from 'src/app/models/consulta.model';
 
 @Component({
   selector: 'consulta-detalhe',
@@ -24,24 +17,13 @@ import { ConsultaService } from 'src/app/services/consulta-service';
 })
 export class ConsultaDetalheComponent extends DetailComponent implements OnInit {
 
-  tiposDocumento: Combobox[];
-
-  documentosFiles: File[] = [];
-  anexosFiles: File[] = [];
-  anexos: Processo_PendenciaAnexo[];
-  documentos: Processo_PendenciaDocumentoSolicitado[];
   loading: boolean = false;
   title: string;
-  situacoesOptions: SituacaoPendenciaEnum[];
-  codSituacoes: any;
-  codPessoaLogada: string;
   salvarLoading: boolean = false;
-  pendencias: PesquisarPendencias = new PesquisarPendencias();
-  pendencia: Processo_Pendencia = new Processo_Pendencia();
-  filter: {
-    anoProcesso?: number,
-    numeroProcesso?: number
-  } = {};
+  consultas: RetornoConsultasDTO = new RetornoConsultasDTO();
+  consulta: Consulta = new Consulta();
+  formType: FormType;
+
 
   @ViewChild(ConsultaDetalheDadosComponent) dadosComponent: ConsultaDetalheDadosComponent;
 
@@ -56,26 +38,7 @@ export class ConsultaDetalheComponent extends DetailComponent implements OnInit 
   }
 
   ngOnInit() {
-    this.initializeOptions();
     this.loadRouteData();
-  }
-
-  /**
-   * Inicializando menus e outros componentes.
-   *
-   */
-  initializeOptions(): void {
-    this.loading = true;
-    this.situacoesOptions = getSituacaoPendenciaAsList(),
-      this.pendenciaService.getTiposDocumento().subscribe(
-        response => {
-          const tiposDocumento: Combobox[] = response;
-          this.tiposDocumento = tiposDocumento['result'];
-          this.loading = false;
-        }, error => {
-          this.loading = false;
-          throw error;
-        });
   }
 
   /**
@@ -84,27 +47,16 @@ export class ConsultaDetalheComponent extends DetailComponent implements OnInit 
    */
   loadRouteData(): void {
     const id: number = Number(this.route.snapshot.paramMap.get('id'));
-    const numProcesso: number = Number(this.route.snapshot.queryParamMap.get('numProcesso'));
-    const anoProcesso: number = Number(this.route.snapshot.queryParamMap.get('anoProcesso'));
     switch (this.formType) {
       case FormType.create:
-        this.title = 'Inclusão de Pendências';
-        this.preencherProcesso(numProcesso, anoProcesso);
+        this.title = 'Inclusão de Consultas';
         break;
       case FormType.update:
-        this.title = 'Alteração de Pendências';
+        this.title = 'Alteração de Consultas';
         this.loadData(id);
         break;
       case FormType.view:
-        this.title = 'Consulta de Pendências';
-        this.loadData(id);
-        break;
-      case FormType.avaliate:
-        this.title = 'Avaliar a Pendência';
-        this.loadData(id);
-        break;
-      case FormType.answer:
-        this.title = 'Responder a Pendência';
+        this.title = 'Consulta de Consultas';
         this.loadData(id);
         break;
     }
@@ -117,21 +69,21 @@ export class ConsultaDetalheComponent extends DetailComponent implements OnInit 
    */
   loadData(id: number): void {
     this.loading = true;
-    this.pendenciaService.getPendenciaById(id).subscribe(
-      pendencia => {
-        this.pendencia = pendencia['result'];
-        this.pendencia.documentosPendentes.forEach(
+    this.consultaService.getConsultaById(id).subscribe(
+      consulta => {
+        this.consulta = consulta['result'];
+        this.consulta.documentosPendentes.forEach(
           (doc: Processo_PendenciaDocumentoSolicitado) => {
             doc.documentoAceito = (doc.situacaoDocumentoSolicitadoId === 1) ? false : true;
             doc.possuiArquivo = (doc.nomeArquivo) ? true : false;
           }
         );
-        this.pendencia.dataCadastro = new Date(pendencia['result'].dataCadastro.toString());
-        this.pendencia.dataPrazo = new Date(pendencia['result'].dataPrazo.toString());
-        this.pendencia.numeroAnoProcessoMask = this.pendencia.numeroProcesso.toString() + "/" + this.pendencia.anoCadastro.toString();
-        this.pendencia.processo = new SIPADProcesso();
-        this.filter.numeroProcesso = this.pendencia.numeroProcesso;
-        this.filter.anoProcesso = this.pendencia.anoCadastro;
+        this.consulta.dataCadastro = new Date(consulta['result'].dataCadastro.toString());
+        this.consulta.dataPrazo = new Date(consulta['result'].dataPrazo.toString());
+        this.consulta.numeroAnoProcessoMask = this.consulta.numeroProcesso.toString() + "/" + this.consulta.anoCadastro.toString();
+        this.consulta.processo = new SIPADProcesso();
+        this.filter.numeroProcesso = this.consulta.numeroProcesso;
+        this.filter.anoProcesso = this.consulta.anoCadastro;
         this.loading = false;
         this.pendenciaService.getProcessoSipad(this.filter).subscribe(
           response => {
@@ -149,57 +101,28 @@ export class ConsultaDetalheComponent extends DetailComponent implements OnInit 
     this.loading = false;
   }
 
-  preencherProcesso(numProcesso: number, anoProcesso: number) {
-    setTimeout(_ => {
-      if (numProcesso && anoProcesso) {
-        this.pendencia.numeroAnoProcessoMask = `${numProcesso}/${anoProcesso}`;
-        this.dadosComponent.findProcessoSipad();
-      }
-    }, 0);
-  }
-
-  /**
-   * Limpa os campos relacionados ao Processo do SIPAD.
-   *
-   */
-  clearProcessoSIPAD(): void {
-    this.pendencia.processo.requerente = null;
-    this.pendencia.processo.dataCadastro = null;
-  }
-
-  /**
-   * Inicializa os campos relacionados ao Processo do SIPAD.
-   *
-   * @param processoSIPAD
-   */
-  initializeProcesso(processoSIPAD: SIPADProcesso): void {
-    this.pendencia.processo.requerente = processoSIPAD.requerente;
-    this.pendencia.processo.dataCadastro = new Date(processoSIPAD.dataCadastro);
-    this.pendencia.situacaoDocumentoDescricao = getSituacaoPendenciaDescById(this.pendencia.situacaoDocumentoId);
-  }
-
   /**
    * Realiza as validações necessárias para inserção ou atualização do registro
    *
    * @param form
    */
   handleValidSubmission(form: NgForm): void {
-    const numAnoProcessoSIPAD: string[] = this.pendencia.numeroAnoProcessoMask ? this.pendencia.numeroAnoProcessoMask.split('/') : [];
-    this.pendencia.numeroProcesso = Number(numAnoProcessoSIPAD[0]);
-    this.pendencia.anoCadastro = Number(numAnoProcessoSIPAD[1]);
+    const numAnoProcessoSIPAD: string[] = this.consulta.numeroAnoProcessoMask ? this.consulta.numeroAnoProcessoMask.split('/') : [];
+    this.consulta.numeroProcesso = Number(numAnoProcessoSIPAD[0]);
+    this.consulta.anoCadastro = Number(numAnoProcessoSIPAD[1]);
 
-    const existeDocPendente = this.pendencia.documentosPendentes.every(documento =>
+    const existeDocPendente = this.consulta.documentosPendentes.every(documento =>
       Boolean(documento.tipoDocumentoId));
 
-    if (this.pendencia.id) {
-      if (!this.pendencia.processo.requerente || !this.pendencia.processo.dataCadastro) {
+    if (this.consulta.id) {
+      if (!this.consulta.processo.requerente || !this.consulta.processo.dataCadastro) {
         this.notificationService.notifyInfo('É necessário informar um processo válido do SIPAD.');
       }
-      else if (this.pendencia.documentosPendentes.length === 0 || !existeDocPendente) {
+      else if (this.consulta.documentosPendentes.length === 0 || !existeDocPendente) {
         this.notificationService.notifyError('É necessário informar ao menos um documento para a pendência.');
       }
       else {
-        this.pendenciaService.validarPendenciaEmAberto(this.pendencia).subscribe(
+        this.pendenciaService.validarPendenciaEmAberto(this.consulta).subscribe(
           response => {
             if (response) {
               this.notificationService.notifyError("Já existe pendência em aberto para o processo SIPAD informado.");
@@ -209,14 +132,14 @@ export class ConsultaDetalheComponent extends DetailComponent implements OnInit 
           });
       }
     } else {
-      if (!this.pendencia.processo.requerente || !this.pendencia.processo.dataCadastro) {
+      if (!this.consulta.processo.requerente || !this.consulta.processo.dataCadastro) {
         this.notificationService.notifyInfo('É necessário informar um processo válido do SIPAD.');
       }
-      else if (this.pendencia.documentosPendentes.length === 0 || !existeDocPendente) {
+      else if (this.consulta.documentosPendentes.length === 0 || !existeDocPendente) {
         this.notificationService.notifyError('É necessário informar ao menos um documento para a pendência.');
       }
       else {
-        this.pendenciaService.validarPendenciaEmAberto(this.pendencia).subscribe(
+        this.pendenciaService.validarPendenciaEmAberto(this.consulta).subscribe(
           response => {
             if (response) {
               this.notificationService.notifyError("Já existe pendência em aberto para o processo informado.");
@@ -240,203 +163,27 @@ export class ConsultaDetalheComponent extends DetailComponent implements OnInit 
       this.confirmDialogService.confirm({
         message: 'Todas as informações preenchidas serão perdidas. Deseja continuar?',
         accept: () => {
-          this.router.navigate(['pendencia']);
+          this.router.navigate(['consulta']);
         },
       });
     } else {
-      this.router.navigate(['pendencia']);
+      this.router.navigate(['consulta']);
     }
   }
 
   /**
-   * Adiciona uma nova pendência no banco de dados.
+   * Adiciona uma nova consulta no banco de dados.
    *
    *
    */
-  addPendencia(): void {
+  addConsulta(): void {
     this.loading = true;
-    const pessoaLogada: number = Number.parseInt(this.codPessoaLogada);
-    this.pendenciaService.addPendencia(this.pendencia, this.anexosFiles, this.documentosFiles, pessoaLogada).subscribe(
+    this.consultaService.addConsulta(this.consulta).subscribe(
       pendencia => {
-        this.pendencia = pendencia;
+        this.consulta = pendencia;
         this.notificationService.notifySuccess('Salvo com sucesso!');
         this.loading = false;
-        this.router.navigate(['pendencia']);
-      }, error => {
-        this.loading = false;
-        throw error;
-      }
-    );
-  }
-
-  /**
-   * Método que inicia a conclusão da avaliação da pendência.
-   *
-   */
-  concluir(): void {
-    this.loading = true;
-    const todosDecididos = this.pendencia.documentosPendentes.every(documento =>
-      Boolean(documento.documentoAceito));
-
-    if (!todosDecididos) {
-      this.confirmDialogService.confirm({
-        message: 'Existem documentos não aceitos na pendência. Deseja criar uma nova pendência com esses documentos?',
-        accept: () => {
-          this.pendencia.novaPendencia = true;
-          this.loading = false;
-          this.concluirPendencia();
-        }, reject: () => {
-          this.pendencia.novaPendencia = false;
-          this.loading = false;
-          this.concluirPendencia();
-        }
-      });
-    } else {
-      this.pendencia.novaPendencia = false;
-      this.loading = false;
-      this.concluirPendencia();
-    }
-  }
-
-  /**
-   *  Termina o processamento da conclusão da Pendência.
-   *
-   */
-  concluirPendencia(): void {
-    this.loading = true;
-    this.pendencia.documentosPendentes.forEach(
-      documento => {
-        documento.situacaoDocumentoSolicitadoId = documento.documentoAceito ? SituacaoDocumentoEnum.ACEITO : SituacaoDocumentoEnum.NAO_ACEITO;
-        documento.dataValidacao = new Date();
-        documento.pessoaValidacaoId = Number.parseInt(this.codPessoaLogada);
-      })
-
-    this.pendencia.situacaoDocumentoId = SituacaoPendenciaEnum.CONCLUIDO;
-    const pessoaLogada: number = Number.parseInt(this.codPessoaLogada);
-    this.pendenciaService.concluirPendencia(this.pendencia, this.anexosFiles, this.documentosFiles, pessoaLogada).subscribe(
-      pendencia => {
-        this.pendencia = pendencia.body;
-        this.pendencia.situacaoDocumentoDescricao = getSituacaoPendenciaDescById(this.pendencia.situacaoDocumentoId);
-        this.loading = false;
-        this.notificationService.notifySuccess('Salvo com sucesso!');
-        this.router.navigate(['pendencia']);
-      }, error => {
-        this.loading = false;
-        throw error;
-      }
-    );
-  }
-
-
-
-  /**
-   * Método que responde a pendência.
-   *
-   */
-  responder(): void {
-    this.loading = true;
-    this.pendencia.dataCiencia = new Date();
-    this.pendencia.pessoaCienciaId = Number.parseInt(this.codPessoaLogada);
-    const pessoaLogada = Number.parseInt(this.codPessoaLogada);
-
-    const existeDocumentoArquitetonico = this.pendencia.documentosPendentes.find(
-      documento => documento.tipoDocumentoId == 166
-    );
-
-    if (existeDocumentoArquitetonico != null || existeDocumentoArquitetonico != undefined) {
-      this.pendenciaService.isUserArq(pessoaLogada).subscribe(
-        response => {
-          if (response) {
-            const todosDocumentosAnexados = this.pendencia.documentosPendentes.every(
-              documento => Boolean(documento.nomeArquivo)
-            );
-            if (!todosDocumentosAnexados) {
-              this.notificationService.notifyError('Existem documentos pendentes sem arquivo de resposta, favor verificar.');
-              this.loading = false;
-            } else {
-              const pessoa: number = Number.parseInt(this.codPessoaLogada);
-              this.pendencia.situacaoDocumentoId = SituacaoPendenciaEnum.EM_ANALISE;
-              this.pendencia.documentosPendentes.forEach(
-                documento => {
-                  documento.situacaoDocumentoSolicitadoId = SituacaoDocumentoEnum.NAO_ACEITO;
-                  documento.dataAnexacao = new Date();
-                  documento.pessoaAnexacaoId = Number.parseInt(this.codPessoaLogada);
-                });
-
-              this.pendenciaService.responderPendencia(this.pendencia, this.anexosFiles, this.documentosFiles, pessoa).subscribe(
-                pendencia => {
-                  //this.pendencia = pendencia.body;
-                  //this.pendencia.situacaoDocumentoDescricao = getSituacaoPendenciaDescById(this.pendencia.situacaoDocumentoId);
-                  this.loading = false;
-                  this.notificationService.notifySuccess('Salvo com sucesso!');
-                  this.router.navigate(['processo']);
-                }, error => {
-                  this.loading = false;
-                  throw error;
-                }
-              );
-            }
-          } else {
-            this.notificationService.notifyError('Somente usuários com CREA ou CAU podem responder pendências que solicitam projetos arquitetônicos.');
-            this.loading = false;
-          }
-        }, error => {
-          this.loading = false;
-          throw error;
-        }
-      )
-    } else {
-      const todosDocumentosAnexados = this.pendencia.documentosPendentes.every(
-        documento => Boolean(documento.nomeArquivo)
-      );
-
-      if (!todosDocumentosAnexados) {
-        this.notificationService.notifyError('Existem documentos pendentes sem arquivo de resposta, favor verificar.');
-        this.loading = false;
-      } else {
-        const pessoa: number = Number.parseInt(this.codPessoaLogada);
-        this.pendencia.situacaoDocumentoId = SituacaoPendenciaEnum.EM_ANALISE;
-        this.pendencia.documentosPendentes.forEach(
-          documento => {
-            documento.situacaoDocumentoSolicitadoId = SituacaoDocumentoEnum.NAO_ACEITO;
-            documento.dataAnexacao = new Date();
-            documento.pessoaAnexacaoId = Number.parseInt(this.codPessoaLogada);
-          });
-
-        this.pendenciaService.responderPendencia(this.pendencia, this.anexosFiles, this.documentosFiles, pessoa).subscribe(
-          pendencia => {
-            //this.pendencia = pendencia.body;
-            //this.pendencia.situacaoDocumentoDescricao = getSituacaoPendenciaDescById(this.pendencia.situacaoDocumentoId);
-            this.loading = false;
-            this.notificationService.notifySuccess('Salvo com sucesso!');
-            this.router.navigate(['processo']);
-          }, error => {
-            this.loading = false;
-            throw error;
-          }
-        );
-      }
-    }
-  }
-
-  /**
-   * Salva o rascunho da avaliação da pendência.
-   *
-   */
-  salvarRascunho(): void {
-    this.loading = true;
-    this.pendencia.documentosPendentes.forEach(
-      documento => {
-        documento.situacaoDocumentoSolicitadoId = documento.documentoAceito ? SituacaoDocumentoEnum.ACEITO : SituacaoDocumentoEnum.NAO_ACEITO;
-      })
-
-    this.pendenciaService.salvarRascunhoPendencia(this.pendencia, this.anexosFiles, this.documentosFiles).subscribe(
-      pendencia => {
-        this.pendencia = pendencia.body;
-        this.pendencia.situacaoDocumentoDescricao = getSituacaoPendenciaDescById(this.pendencia.situacaoDocumentoId);
-        this.loading = false;
-        this.notificationService.notifySuccess('Salvo com sucesso!');
-        this.router.navigate(['pendencia']);
+        this.router.navigate(['consulta']);
       }, error => {
         this.loading = false;
         throw error;
